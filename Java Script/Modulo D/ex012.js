764e7fc26062cd98e09837d031f3cb1c92e5503b
Ravynos <th.sobrinho@gmail.com>
2025-02-05 16:20:49 -0300

    Aula 12 - Condições - Pt 2

---
+++ Java Script/Modulo D/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora =  agora.getHours()
+var minuto = agora.getMinutes()
+console.log(`Agora são exatamento ${hora}:${minuto} horas`)
+
+if (hora< 12) {
+    console.log('Bom dia!!!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!!!')
+} else {
+    console.log('Boa noite!!!')
+}
