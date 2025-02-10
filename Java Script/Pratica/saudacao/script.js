ae6f00e7fe55546a6f9beeb5261a46d313b1386d
Ravynos <th.sobrinho@gmail.com>
2025-02-10 16:36:24 -0300

    pratica 3

--- Java Script/Pratica/saudacao/script.js
+++ Java Script/Pratica/saudacao/script.js
@@ -1,20 +1,26 @@
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
+    var saud = document.getElementById('saudacao')
     var data = new Date()
     //var hora = data.getHours()
     var min = data.getMinutes()
-    var hora = 6
+    var hora = 22
     msg.innerHTML = `Agora são ${hora}: ${min}`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
+        saud.innerHTML = "Bom dia!!!"
         img.src = 'Manhã.jpg'
-        document.body.style.background = '#e3dacb'
+        document.body.style.background = '#ffc265'
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE!
+        saud.innerHTML = "Boa tarde!!!"
         img.src = 'tarde.jpg'
+        document.body.style.background = '#ebebe9'
     }else {
         //BOA NOITE!
+        saud.innerHTML = "Boa noite!!!"
         img.src = "noite.jpg"
+        document.body.style.background = '#0f496f'
     }
 }
