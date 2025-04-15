656ba8101e949c2ee1bbc08585328a380d39f540
Ravynos <th.sobrinho@gmail.com>
2025-04-15 16:27:47 -0300

    Exercicios 01 e 02

---
+++ Python/Mundo 1/Exercicios/ex002.py
@@ -0,0 +1,7 @@
+""" Desafio 002 """
+
+""" Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas. """
+
+nome = input('Qual seu nome? ')
+
+print(f'Seja bem vindo, {nome}')
